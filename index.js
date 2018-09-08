640496fbcd97d8275c79d338cf138c2363adfa38
ist188123 <paulorosario@sapo.pt>
2018-09-09 00:47:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+client.on("message", async (msg) => {
+  
+  
+  if (msg.content.startsWith('%')) {
+    msg.reply("oi");
+    
+  }
+  
+  
+  
+  
+  });
+
+client.login(process.env.BOT_TOKEN);
