da8317c8eb2e0c88d2c9ba21a91007e03c416cb0
ist188123 <paulorosario@sapo.pt>
2018-09-09 01:24:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on("message", async (msg) => {
   
   
   
-  if(msg.content.startsWith("//inrole")){
+  if(msg.content.startsWith("//chansey")){
     let roleName = msg.content.split(" ").slice(1).join(" ");
 
     //Filtering the guild members only keeping those with the role
