fe82d5549e72255266cffdfc9e24c1503cc65cce
ist188123 <paulorosario@sapo.pt>
2018-09-09 17:49:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on("message", async (msg) => {
   
   
     
-
+let notifica==0;
   
   if(msg.content.startsWith('5ovos')){
    
@@ -35,13 +35,12 @@ let roleName ="CHANSEY";
   }).map(member => {
     //msg.guild.channels.find("name", "quest-info").sendMessage('\@'+member.user.username)
     // msg.guild.channels.find("name", "quest-info").sendMessage(client.users.find(member.user.username, "mensagem").toString())
-     // msg.channel.send("Hey " +member.user  + " how's it going?"+msg.author+"https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074");
-   
     
     
     
     
-     
+    
+    
             const embed = new Discord.RichEmbed()
                 .setTitle("POKETSTOP "+pokestop)
                 .setAuthor("Olá, "+member.user.username, "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
@@ -49,8 +48,8 @@ let roleName ="CHANSEY";
                  * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
                  */
                 .setColor(0x00AE86)
-                .setDescription("Publicado por :"+msg.author)
-                .setFooter("Notificado "+member.user  +", ", "https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
+                .setDescription("Quest - Chocar 5 ovos.\nPublicado por :"+msg.author)
+                .setFooter("Notificado ", ", "https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
 
 
                 .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000043-cd4a1ce43f/450/chansey.png")
@@ -58,10 +57,12 @@ let roleName ="CHANSEY";
                 .setTimestamp();
 
 
-
+       if(notifica==0){
             msg.guild.channels.find("name", "quest-info").sendMessage({ embed });
-    
-    
+         notifica==1;
+       }
+            msg.channel.send("Notificado: " +member.user  );
+   
     
     
     
