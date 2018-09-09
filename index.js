35b72ccf7e5e0d4e6b52dcbcfd45ef2cbbd6ed94
ist188123 <paulorosario@sapo.pt>
2018-09-09 17:15:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on("message", async (msg) => {
   //----
   
   
-    if (msg.content.startsWith('5ovos')) {
+    if (msg.content.startsWith('chansey')) {
    
     // let roleName = msg.content.split(" ").slice(1).join(" ");
     let roleName = "5ovos";
@@ -32,7 +32,7 @@ client.on("message", async (msg) => {
       //msg.guild.channels.find("name", "quest-info").sendMessage('\@'+member.user.username)
       // msg.guild.channels.find("name", "quest-info").sendMessage(client.users.find(member.user.username, "mensagem").toString())
       // msg.channel.send("Hey " +member.user  + " how's it going?"+msg.author+"https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074");
-      msg.reply(msg.content);
+     
 
       msg.guild.channels.find("name", "quest-info").sendMessage(member.user);
 
