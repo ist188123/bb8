c82a7cae489f583249f80db32de514bc0a8193e1
ist188123 <paulorosario@sapo.pt>
2018-09-09 13:07:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,30 @@ client.on("message", async (msg) => {
   
 
   
+  //----
+  
+  
+  if(msg.content.startsWith('5ovos')){
+  let roleName = msg.content.split(" ").slice(1).join(" ");
+
+  //Filtering the guild members only keeping those with the role
+  //Then mapping the filtered array to their usernames
+  let membersWithRole = msg.guild.members.filter(member => { 
+     
+      return member.roles.find("name", roleName);
+  }).map(member => {
+    //msg.guild.channels.find("name", "quest-info").sendMessage('\@'+member.user.username)
+    // msg.guild.channels.find("name", "quest-info").sendMessage(client.users.find(member.user.username, "mensagem").toString())
+      msg.channel.send("Hey " +member.user  + " how's it going?"+msg.author);
+   
+  })
+
+
+}
   
   
   
-  
-  
-  
-  
-  
+  //----
   
   
   
