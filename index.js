2c8ec5d90c3da53e021daea851df1986fe9a95e1
ist188123 <paulorosario@sapo.pt>
2018-09-09 17:07:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,24 +38,11 @@ let roleName ="5ovos";
     
     
      
-            const embed = new Discord.RichEmbed()
-                .setTitle("QUEST NOTIFICAÇÃO")
-                .setAuthor("Olá, "+member.user, "https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
-                /*
-                 * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-                 */
-                .setColor(0x00AE86)
-                .setDescription("Hey " +member.user  + " how's it going?"+msg.author+"\nhttps://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
-                .setFooter("Desenvolvido por Damasc010 - Pinhal Novo, pubicado ", "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
-
-
-                .setThumbnail("")
-
-                .setTimestamp();
+            
 
 
 
-            msg.guild.channels.find("name", "quest-info").sendMessage({ embed });
+            msg.guild.channels.find("name", "quest-info").sendMessage(member.user);
     
     
     
