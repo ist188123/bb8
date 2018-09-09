af8c328c96819aef6cb3559deaf5e664d442bb41
ist188123 <paulorosario@sapo.pt>
2018-09-09 13:21:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,42 @@ let roleName ="5ovos";
   }).map(member => {
     //msg.guild.channels.find("name", "quest-info").sendMessage('\@'+member.user.username)
     // msg.guild.channels.find("name", "quest-info").sendMessage(client.users.find(member.user.username, "mensagem").toString())
-      msg.channel.send("Hey " +member.user  + " how's it going?"+msg.author+"https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074");
+     // msg.channel.send("Hey " +member.user  + " how's it going?"+msg.author+"https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074");
    
+    
+    
+    
+    
+     
+            const embed = new Discord.RichEmbed()
+                .setTitle("INFORMAÇÃO")
+                .setAuthor("Olá treinadores.", "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
+                /*
+                 * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+                 */
+                .setColor(0x00AE86)
+                .setDescription("Hey " +member.user  + " how's it going?"+msg.author)
+                .setFooter("Desenvolvido por Damasc010 - Pinhal Novo, pubicado ", "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
+
+
+                .setThumbnail("https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
+
+                .setTimestamp();
+
+
+
+            msg.guild.channels.find("name", "quest-info").sendMessage({ embed });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   })
 
 
