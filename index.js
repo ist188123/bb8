52f39d299f1cbd3463ed1f9775d30623a63633a2
ist188123 <paulorosario@sapo.pt>
2018-09-09 17:42:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,14 +43,14 @@ let roleName ="CHANSEY";
     
      
             const embed = new Discord.RichEmbed()
-                .setTitle("POKETSTOP "+pokestop.toUpperCase)
-                .setAuthor("Olá, "+member.user, "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
+                .setTitle("POKETSTOP "+pokestop)
+                .setAuthor("Olá, "+member.user.username, "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
                 /*
                  * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
                  */
                 .setColor(0x00AE86)
                 .setDescription("Publicado por :"+msg.author)
-                .setFooter("Desenvolvido por Damasc010 - Pinhal Novo, pubicado ", "https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
+                .setFooter("Notificado "+member.user  +", ", "https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
 
 
                 .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000043-cd4a1ce43f/450/chansey.png")
