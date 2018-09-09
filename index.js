f5d9b98b0691ac9dfbe2ea1d2dd34b88a51e07e9
ist188123 <paulorosario@sapo.pt>
2018-09-09 18:20:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,15 +41,15 @@ let roleName ="CHANSEY";
     
     
     
-            const embed = new Discord.RichEmbed()
+           const embed = new Discord.RichEmbed()
                 .setTitle("POKETSTOP "+pokestop)
-                .setAuthor("Olá, "+member.user.username, "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
+                .setAuthor("Olá, "+member.user, "https://exraidspinhalnovo.webnode.pt/_files/200000022-231042409e/200/damasc010.png")
                 /*
                  * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
                  */
                 .setColor(0x00AE86)
-                .setDescription("Quest - Chocar 5 ovos.\nPublicado por :"+msg.author)
-                .setFooter("Notificado ", ", https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
+                .setDescription("Publicado por :"+msg.author)
+                .setFooter("Pubicado ", "https://www.google.com/maps/place/38°38'08.5%22N+8°54'29.2%22W/@38.635691,-8.9098598,17z/data=!3m1!4b1!4m6!3m5!1s0x0:0x0!7e2!8m2!3d38.635691!4d-8.9081074")
 
 
                 .setThumbnail("https://exraidspinhalnovo.webnode.pt/_files/200000043-cd4a1ce43f/450/chansey.png")
@@ -57,7 +57,7 @@ let roleName ="CHANSEY";
                 .setTimestamp();
 
 
-      
+
             msg.guild.channels.find("name", "quest-info").sendMessage({ embed });
          
           
