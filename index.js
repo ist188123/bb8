0f8f00cf37e5b359afc9a51d49af48ddc4df79af
ist188123 <paulorosario@sapo.pt>
2019-06-12 11:49:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ if (msg.content.startsWith('!quest')) {
                  for (var x = 0; x < questMap.length; x++) {
   //  msg.sendMessage(questMap[x].cod)
                   
-       pppp=pppp+"<:pkst:585903628817137785>"+questMap[x].cod+"\n"+questMap[x].missao.trim()+"\n"
+       pppp=pppp+questMap[x].cod+"\n"+questMap[x].missao.trim()+"\n"
        
     
     
