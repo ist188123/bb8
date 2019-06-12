4e4289bc490880792d3eb256907bcffc6056f243
ist188123 <paulorosario@sapo.pt>
2019-06-12 12:04:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -56,54 +56,7 @@ if (msg.content.startsWith('!quest')) {
              //------
               
              
-	const exampleEmbed = {
-	color: 0x0099ff,
-	title: 'Some title',
-	url: 'https://discord.js.org',
-	author: {
-		name: 'Some name',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png',
-		url: 'https://discord.js.org',
-	},
-	description: 'Some description here',
-	thumbnail: {
-		url: 'https://i.imgur.com/wSTFkRM.png',
-	},
-	fields: [
-		{
-			name: 'Regular field title',
-			value: 'Some value here',
-		},
-		{
-			name: '\u200b',
-			value: '\u200b',
-		},
-		{
-			name: 'Inline field title',
-			value: 'Some value here',
-			inline: true,
-		},
-		{
-			name: 'Inline field title',
-			value: 'Some value here',
-			inline: true,
-		},
-		{
-			name: 'Inline field title',
-			value: 'Some value here',
-			inline: true,
-		},
-	],
-	image: {
-		url: 'https://i.imgur.com/wSTFkRM.png',
-	},
-	timestamp: new Date(),
-	footer: {
-		text: 'Some footer text here',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png',
-	},
-};
-
+	
               
               
            //-----   
@@ -119,12 +72,12 @@ if (msg.content.startsWith('!quest')) {
     
     }
       
-      msg.channel.send(exampleEmbed);
+     
     
-  //     msg.channel.send({embed: {
-//  color: 3447003,
-//  description: "QUEST DISPONIVEIS\n"+pppp
-//}});   
+     msg.channel.send({embed: {
+ color: 3447003,
+ description: "QUEST DISPONIVEIS\n"+pppp
+}});   
               
     //fim mensagem --        
             });
